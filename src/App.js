c5f070676f31df48c42473a899d0e0209784fe2d
Elliot Charney <emc@hey.com>
2021-12-30 16:54:47 -0800

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="flex-container navbar">
+          <Link to="/">
+            <span className="brand">eleanor rykener</span>
+          </Link>
+          <div className="flex-container">
+            <Link to="/shop">shop</Link>
+            <Link to="/about">about</Link>
+            <Link to="/contact">contact</Link>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="main">
+        <h1>coming soon</h1>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/shop">
+            <Shop />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Contact() {
+  return <h2>Contact</h2>;
+}
+
+function Shop() {
+  return <h2>Shop</h2>;
 }
 
 export default App;
