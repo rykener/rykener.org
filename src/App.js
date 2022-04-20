0cbcb23344535d1adf8401c35ebad37defea5052
Elliot Charney <emc@hey.com>
2022-04-20 16:03:55 -0700

    wipe store

--- src/App.js
+++ src/App.js
@@ -1,57 +1,12 @@
 import './App.css';
-import About from './About.js';
-import Contact from './Contact.js';
-import Shop from './Shop.js';
-import Zine from './Zine.js';
-import Explanations from './Explanations.js';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <div className="flex-container navbar">
-          <Link to="/">
-            <span className="brand">eleanor rykener</span>
-          </Link>
-          <div className="flex-container">
-            <Link to="/shop">shop</Link>
-            <Link to="/about">about</Link>
-            <Link to="/contact">contact</Link>
-            <a href="https://instagram.com/eleanor.rykener">instagram</a>
-          </div>
-        </div>
-      </div>
-
-
-      <div className="main">
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="/shop">
-            <Shop />
-          </Route>
-          <Route path="/zine">
-            <Zine />
-          </Route>
-          <Route path="/explain">
-            <Explanations />
-          </Route>
-          <Route path="/">
-            <Shop />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="main">
+      <h1>Eleanor Rykener</h1>
+      <h4><i>A 14th Century Transgender Icon</i></h4>
+      <p>Learn about her life on <a href="https://en.wikipedia.org/wiki/John/Eleanor_Rykener">Wikipedia</a></p>.
+    </div>
   );
 }
 
