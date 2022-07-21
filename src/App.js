c69b0d4e55e03eb67cbe3f6555fd6e25cf9e0e06
Elliot Charney <emc@hey.com>
2022-07-21 13:44:43 -0700

    restore explanations

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
+import Explanations from './Explanations.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="main">
-      <h1>Eleanor Rykener</h1>
-      <h4><i>A 14th Century Transgender Icon</i></h4>
-      <p>Learn about her life on <a href="https://en.wikipedia.org/wiki/John/Eleanor_Rykener">Wikipedia</a></p>.
-    </div>
+    <Router>
+      <div className="main">
+        <h1>Eleanor Rykener</h1>
+        <h4><i>A 14th Century Transgender Icon</i></h4>
+        <p>Learn about her life on <a href="https://en.wikipedia.org/wiki/John/Eleanor_Rykener">Wikipedia</a></p>.
+      </div>
+      <div className="main">
+        <Switch>
+          <Route path="/explain">
+            <Explanations />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
