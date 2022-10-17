86cff6b6509aba88808f5e38096c090b95e74ff9
Eleanor Rykener <eleanor@rykener.org>
2022-10-16 22:39:15 -0700

    remove text

--- src/App.js
+++ src/App.js
@@ -13,19 +13,6 @@ function App() {
         <h1>Eleanor Rykener</h1>
         <h4><i>A 14th Century Transgender Icon</i></h4>
         <p>Learn about her life on <a href="https://en.wikipedia.org/wiki/John/Eleanor_Rykener">Wikipedia</a></p>.
-        <br/>
-        <br/>
-        <p>
-          rykener.org has borrowed this historically important woman's name,
-          and works towards selling the artwork of trans women in order to
-          donate profits toward mutual aid for other trans women in need.
-        </p>
-        <p>
-          This is not a registered 501(c)(3) but instead a self funded mutual
-          aid organization. When an artist sells their artwork they retain the
-          cost of materials alone. A significant number of trans women live in
-          poverty, and this is our meager way of lending support.
-        </p>
       </div>
       <div className="main">
         <Switch>
