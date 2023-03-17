3ac7d95713f54d46d21dcf00406bd79796f34b90
rykener <emc@hey.com>
2023-03-17 13:23:44 -0700

    delete text

--- src/App.js
+++ src/App.js
@@ -10,9 +10,6 @@ function App() {
   return (
     <Router>
       <div className="main">
-        <h1>Eleanor Rykener</h1>
-        <h4><i>A 14th Century Transgender Icon</i></h4>
-        <p>Learn about her life on <a href="https://en.wikipedia.org/wiki/John/Eleanor_Rykener">Wikipedia</a></p>.
       </div>
       <div className="main">
         <Switch>
